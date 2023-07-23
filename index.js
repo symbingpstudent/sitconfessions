eb7086bf60cda8fd7b125d17c83036035fd3249c
sitian_ngp <139035346+symbingpstudent@users.noreply.github.com>
2023-07-23 10:38:04 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const messages = document.getElementById("messages"),
       text = document.getElementById("text"),
       submit = document.getElementById("submit");
-const a =["crush","lover","love","hate","boyfriend","girlfriend","bf","gf","fuck","mota","bitch","suck","sex","sexy","cute","sexy"];
-var flag = 0;
-function word_checker(){
-    for (let i=0;i<a.length;i++){
-        if (text.value.toLowerCase().includes(a[i])){
-            flag = 1;
-            break;
-        }
-    }
-    if (flag == 1){
-        alert("sorry due to unusual words your message is blocked and not forwarded to the admin");
-        window.location.reload();
-    }   
-    else{
-        messages.innerHTML = "<h3>your message is forwarded to the admin</h3>";
+// const a =["crush","lover","love","hate","boyfriend","girlfriend","bf","gf","fuck","mota","bitch","suck","sex","sexy","cute"];
+// var flag = 0;
+// function word_checker(){
+//     for (let i=0;i<a.length;i++){
+//         if (text.value.toLowerCase().includes(a[i])){
+//             flag = 1;
+//             break;
+//         }
+//     }
+//     if (flag == 1){
+//         alert("sorry due to unusual words your message is blocked and not forwarded to the admin");
+//         window.location.reload();
+//     }   
+//     else{
+//         messages.innerHTML = "<h3>your message is forwarded to the admin</h3>";
         
+//         Email.send({
+//             SecureToken : "a33ab415-b48e-4439-9642-7635e19d47bd",
+//             To : 'symbinagpur@gmail.com',
+//             From : "fridayro706@gmail.com",
+//             Subject : "confession",
+//             Body : text.value
+//         }).then(
+//           message => alert(message)
+//         );
+//     }
+// }
+function send(){
+        messages.innerHTML = "<h3>your message is forwarded to the admin</h3>";
         Email.send({
             SecureToken : "a33ab415-b48e-4439-9642-7635e19d47bd",
             To : 'symbinagpur@gmail.com',
@@ -26,6 +39,7 @@ function word_checker(){
         }).then(
           message => alert(message)
         );
-    }
+        
+        
 }
-submit.addEventListener("click",word_checker);
+submit.addEventListener("click",send);
